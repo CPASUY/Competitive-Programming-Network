4ec2cc6510719ffb9e45dd79f7aee92aad27ad4d
Carolina Pasuy <carolinapasuy@hotmail.com>
2021-09-05 14:35:23 -0500

    Redirecionamiento de las paginas

--- Models/Competitor.cs
+++ Models/Competitor.cs
@@ -10,10 +10,26 @@ namespace Rcp.Models
     public class Competitor
     {
         public int ID { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Username")]
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "ConfirmPwd")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="Confirm password does not match")]
         public string ConfirmPwd { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
         public string LastName { get; set; }
 
         [DataType(DataType.Date)]
---
+++ Models/SeedData.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Rcp.Data;
+using Rcp.Models;
+using System;
+using System.Linq;
+
+namespace Rcp
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RcpContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RcpContext>>()))
+            {
+                // Look for any movies.
+                if (context.Competitor.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Competitor.AddRange(
+                    new Competitor
+                    {
+                        Username = "Seyerman",
+                        FirstName = "Juan Manuel",
+                        LastName = "Reyes",
+                        Password = "1234",
+                        ConfirmPwd= "1234",
+                        Birthdate= DateTime.Parse("1995-4-01"),
+                    },
+
+                    new Competitor
+                    {
+                        Username = "Favellaneda",
+                        FirstName = "Fabio",
+                        LastName = "Avellaneda",
+                        Password = "123",
+                        ConfirmPwd = "123",
+                        Birthdate = DateTime.Parse("1987-9-06"),
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
